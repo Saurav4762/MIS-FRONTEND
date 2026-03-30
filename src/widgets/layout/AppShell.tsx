934bf12339708473110d7b8121779e455816a1d2
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-03-30 17:24:20 +0545

    Update UI: add breadcrumbs

--- src/widgets/layout/AppShell.tsx
+++ src/widgets/layout/AppShell.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "@tanstack/react-router";
+import Breadcrumb from "@widgets/breadcrumbs";
 import SidebarNavMenu from "@widgets/navigation";
 
 export function AppShell() {
@@ -6,7 +7,10 @@ export function AppShell() {
     <div className="flex min-h-screen bg-[#020816] text-white">
       <SidebarNavMenu />
 
-      <main className="min-w-0 flex-1 p-6">
+      <main className="min-w-0 flex-1 p-6 space-y-6.5">
+        <div className="pb-6">
+          <Breadcrumb />
+        </div>
         <Outlet />
       </main>
     </div>
