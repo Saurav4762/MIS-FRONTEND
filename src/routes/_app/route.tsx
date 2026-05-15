b8c6f3078c08ad22e3d57e9d44b97c4919cc5bd4
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-05-15 22:21:48 +0545

    fix(app): adjust app layout shell for draft form views

--- src/routes/_app/route.tsx
+++ src/routes/_app/route.tsx
@@ -9,11 +9,11 @@ function RouteComponent() {
   return (
     <div className="flex min-h-screen h-dvh overflow-hidden bg-(--mis-color-ink-50) text-(--mis-color-ink-900)">
       <SidebarNavMenu />
-      <main className="min-w-0 flex-1 p-6 space-y-6.5 overflow-y-scroll">
+      <main className="min-w-0 flex-1 px-6 flex flex-col pt-6 space-y-6.5 overflow-y-scroll">
         <div className="pb-6 pt-0.5">
           <Breadcrumbs />
         </div>
-        <div>
+        <div className="flex-1 overflow-hidden">
           <Outlet />
         </div>
       </main>
