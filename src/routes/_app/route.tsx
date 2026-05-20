2a97ef9b855cb054b4578c2b8b5004d88380a3c1
chirag-bimali <70309029+chirag-bimali@users.noreply.github.com>
2026-05-20 08:54:14 +0545

    Hide scroll bar

--- src/routes/_app/route.tsx
+++ src/routes/_app/route.tsx
@@ -9,7 +9,7 @@ function RouteComponent() {
   return (
     <div className="flex min-h-screen h-dvh overflow-hidden bg-(--mis-color-ink-50) text-(--mis-color-ink-900)">
       <SidebarNavMenu />
-      <main className="min-w-0 flex-1 px-6 flex flex-col pt-6 space-y-6.5 overflow-y-scroll">
+      <main className="min-w-0 flex-1 px-6 flex flex-col pt-6 space-y-6.5 overflow-y-auto">
         <div className="pb-6 pt-0.5">
           <Breadcrumbs />
         </div>
